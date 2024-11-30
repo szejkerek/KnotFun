f8ebec86c693c3c51bc9abb119730ace2db814a0
julibug <72342559+julibug@users.noreply.github.com>
2024-11-30 23:46:42 +0100

    enviroment on level 1

--- Assets/Credits.cs
+++ Assets/Credits.cs
@@ -5,6 +5,13 @@ public class Credits : MonoBehaviour
     [SerializeField] GameObject menu;
     [SerializeField] GameObject credits;
 
+    private Vector3 lastPosition;
+
+    private void Start()
+    {
+        lastPosition = transform.position;
+    }
+
     public void ChangeToCredits()
     {
         menu.SetActive(false);
