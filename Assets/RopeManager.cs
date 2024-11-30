5459b938c33df2538f47efec300efac0ef13b046
Bartek <bartekk.gordon@gmail.com>
2024-11-30 22:01:07 +0100

    Update

--- Assets/RopeManager.cs
+++ Assets/RopeManager.cs
@@ -1,3 +1,4 @@
+using System;
 using GogoGaga.OptimizedRopesAndCables;
 using UnityEngine;
 
@@ -5,20 +6,39 @@ public class RopeManager : MonoBehaviour
 {
     private Rope rope;
     [SerializeField] private GameObject ropeLoop;
-    
-    private float currentRopeLenght;
+    [SerializeField] private Vector2 ropeRenderLength;
+    [SerializeField] private Vector2 ropePullDistance;
+    public float currentRopeLength;
+    public float currentPullDistance;
 
-    public void SetVisiblebool(bool active)
+    [SerializeField] private float interpolationDuration = 0.2f;
+
+    private void Awake()
     {
-        rope.gameObject.SetActive(active);
-        rope.RecalculateRope();
+        rope = GetComponent<Rope>();
+        ChangeRopeLenght(0.5f);
     }
-    
-    public void ChangeLineLenght(float lenght)
+
+    private void Update()
     {
-        StopAllCoroutines();
-        currentRopeLenght = lenght;
-        rope.ropeLength = currentRopeLenght;
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            ChangeRopeLenght(0f);
+            Debug.Log("0");
+        }
+        else if (Input.GetKeyDown(KeyCode.F2))
+        {
+             ChangeRopeLenght(1);
+             Debug.Log("1");
+        }
+    }
+
+    public void ChangeRopeLenght(float t)
+    {
+        currentRopeLength = Mathf.Lerp(ropeRenderLength.x, ropeRenderLength.y, t);
+        currentPullDistance = Mathf.Lerp(ropePullDistance.x, ropePullDistance.y, t);
+        
+        rope.ropeLength = currentRopeLength;
         rope.RecalculateRope();
     }
 }
