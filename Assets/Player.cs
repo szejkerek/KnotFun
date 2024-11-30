54e453b8c2570c09a8c494865f90b580df6d0659
juliwro851 <72439273+juliwro851@users.noreply.github.com>
2024-11-30 23:24:50 +0100

    Update Player.cs

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,13 @@
 using GogoGaga.OptimizedRopesAndCables;
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.InputSystem.XInput;
+ 
 
 public class Player : MonoBehaviour
 {
@@ -7,12 +15,24 @@ public class Player : MonoBehaviour
     private CharacterController characterController;
     public PlayerAttackManager PlayerAttackManager {get; private set;}
 
+    [SerializeField]
+    public int playerNumber = 1;
+
+    public ReadOnlyArray<Gamepad> gamepads = Gamepad.all;
+
+    //input z kontrollera
+
     public Rope PlayerRope;
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+<<<<<<< Updated upstream
         PlayerAttackManager = GetComponent<PlayerAttackManager>();
+=======
+        gamepads = Gamepad.all;
+
+>>>>>>> Stashed changes
     }
 
     public void Move()
@@ -20,14 +40,57 @@ public class Player : MonoBehaviour
         characterController.Move(currentDirection);
     }
 
+
+
     public Vector3 GetMovementDirection(KeyCode up, KeyCode down, KeyCode left, KeyCode right)
     {
-        Vector3 moveDirection = Vector3.zero;
-        if (Input.GetKey(up)) moveDirection += Vector3.forward;
-        if (Input.GetKey(down)) moveDirection += Vector3.back;
-        if (Input.GetKey(left)) moveDirection += Vector3.left;
-        if (Input.GetKey(right)) moveDirection += Vector3.right;
 
-        return moveDirection;
+        if(playerNumber == 1)
+        {
+            Vector3 moveDirection = Vector3.zero;
+            Vector2 moveInput = gamepads[0].leftStick.ReadValue().normalized;
+            moveDirection += new Vector3(moveInput.x, 0f, moveInput.y);
+            return moveDirection;
+        }
+
+        else if (playerNumber == 2)
+        {
+            Vector3 moveDirection = Vector3.zero;
+            Vector2 moveInput = gamepads[1].leftStick.ReadValue().normalized;
+            moveDirection += new Vector3(moveInput.x, 0f, moveInput.y);
+            return moveDirection;
+        }
+
+        else
+        {
+            Vector3 moveDirection = Vector3.zero;
+            if (Input.GetKey(up)) moveDirection += Vector3.forward;
+            if (Input.GetKey(down)) moveDirection += Vector3.back;
+            if (Input.GetKey(left)) moveDirection += Vector3.left;
+            if (Input.GetKey(right)) moveDirection += Vector3.right;
+
+            return moveDirection;
+        }
+
     }
+
+    public void Update()
+    {
+        if(gamepads[0].rightTrigger.ReadValue() > 0.1f)
+        {
+            Debug.Log("Prawy trigger 1 gracz");
+        }
+        if (gamepads[1].rightTrigger.ReadValue() > 0.1f)
+        {
+            Debug.Log("Prawy trigger 2 gracz");
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Spacja 3 gracz");
+        }
+    }
+
+
+
+    //czy jest klikany trigger albo spacja 
 }
