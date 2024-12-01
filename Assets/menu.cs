b2d653b554b5649bacf451bde46ecf7181442678
juliwro851 <72439273+juliwro851@users.noreply.github.com>
2024-12-01 09:53:54 +0100

    scene changes

---
+++ Assets/menu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public  void ButtonUi()
+    {
+        Debug.Log("Level 1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //Debug.Log(Time.timeScale);
+    }
+}
